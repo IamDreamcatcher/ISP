975422ca4f2f75b2aeff9b706f7210eb3ec7457a
dream <OlegVolkovskiy2002@yandex.ru>
2022-04-22 12:12:19 -0700

    add match

--- Lab2/lib/serialization/custom_serialization.py
+++ Lab2/lib/serialization/custom_serialization.py
@@ -4,12 +4,42 @@ import lib.lib_constants
 def serialize(obj):
     data = {}
     obj_type = type(obj)
-    obj_type_name = obj.__name__
-    if obj_type in (int, float, str, bool, complex):
-        return obj
-    elif obj_type in (tuple, list):
-        data[lib.lib_constants.TYPE] = obj_type_name
-        data[lib.lib_constants.VALUE] = [serialize(cur) for cur in obj]
+    obj_type_name = obj_type.__name__
+    data[lib.lib_constants.TYPE] = obj_type_name
+
+    match obj_type_name:
+        case 'int' | 'float' | 'bool' | 'complex' | 'str':
+            data[lib.lib_constants.VALUE] = obj
+            return data
+        case 'list' | 'tuple' | 'bytes':
+            data[lib.lib_constants.VALUE] = [serialize(cur) for cur in obj]
+            return data
+        case 'dict':
+            data[lib.lib_constants.VALUE] = []
+            for cur_key, cur_value in obj.items():
+                key = serialize(cur_key)
+                value = serialize(cur_value)
+                data[lib.lib_constants.VALUE].append([key, value])
+            return data
+        case 'function':
+            data[lib.lib_constants.VALUE] = serialize_function()
+            return data
+        case 'type':
+            data[lib.lib_constants.VALUE] = serialize_class()
+        case _:
+            pass
+
+    print(obj_type_name)
+
+    return data
+
+
+def serialize_function():
+    pass
+
+
+def serialize_class():
+    pass
 
 
 def deserialize(obj):
--- Lab2/main.py
+++ Lab2/main.py
@@ -1,14 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from lib.serialization.custom_serialization import serialize
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class f:
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def out():
+    print(serialize(1))
+    #print(serialize(f))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    out()
